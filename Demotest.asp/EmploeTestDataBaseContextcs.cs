52ad4be0bb99f2ab20bf6cb721d99bd6bb4c4531
RajithaPalla229 <pallarajitha2004.fullstack@gmail.com>
2026-01-07 18:56:23 +0530

    Add project files.

---
+++ Demotest.asp/EmploeTestDataBaseContextcs.cs
@@ -0,0 +1,30 @@
+﻿using DemoApp.APIs.Entities;
+using DemoTest.ASP;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+
+
+namespace DemoApp.APIs
+{
+    public class EmploeTestDataBaseContext : DbContext
+    {
+
+
+
+        //options connection realted details
+        public EmploeTestDataBaseContext(DbContextOptions<EmploeTestDataBaseContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+
+        
+        //base
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ Demotest.asp/Program.cs
@@ -0,0 +1,27 @@
+using DemoApp.APIs;
+using Microsoft.EntityFrameworkCore;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+
+builder.Services.AddDbContext<EmploeTestDataBaseContext>(
+    options =>
+    options.
+    UseSqlServer(builder.Configuration.GetConnectionString("EmploeTestConnection")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
